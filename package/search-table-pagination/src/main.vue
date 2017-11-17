b56f721e63657d4f1dbdb6bbf62d355a835dd48f
Kiko Seijo <kikoseijo@users.noreply.github.com>
2017-11-17 04:22:48 +0100

    row click handler
    Convenient method for when no selection active.

--- package/search-table-pagination/src/main.vue
+++ package/search-table-pagination/src/main.vue
@@ -26,7 +26,7 @@
       :row-class-name="rowClassName"
       style="width: 100%;margin-top:20px;"
       @select="selectHandler" @select-all="selectAllHandler"
-      @selection-change="selectionChangeHandler">
+      @selection-change="selectionChangeHandler" @row-click="clickHandler">
 
       <slot name="prepend" />
 
@@ -376,6 +376,9 @@
       },
       selectionChangeHandler() {
         this.$emit('selection-change', arguments)
+      },
+      clickHandler() {
+        this.$emit('row-click', arguments)
       },
       loadLocalData(data) {
         if (!data) {
